16645ed7178e4c36bc03f136545c089e8f8cc568
LucasAMoreira <39498462+LucasAMoreira@users.noreply.github.com>
2020-12-05 22:43:24 -0300

    Update gerar.js

--- gerar.js
+++ gerar.js
@@ -6,6 +6,7 @@ var tipo="";
 var cenario="";
 var duracao="";
 var instrumento="";
+var andamento="";
 
 //Funções set. Recebem do HTML (gerar.html) as opções do usuário.
 function setTipo(tipo){
@@ -20,12 +21,25 @@ function setInstrumento(instrumento){
 function setDuracao(){
      duracao=document.getElementById("duracao");
      this.duracao=duracao.value;
-}                     
+}      
+function setAndamento(andamento){
+     this.andamento=andamento;
+}               
+
+//Avisa para usuário qual é o instrumento selecionado
+function mostraInstrumento(){
+     var selecionado=document.getElementById("selecionado");
+     selecionado.innerHTML="Instrumento selecionado: <strong>"+this.instrumento+"</strong>";
+}
 
 // Gera String com os parâmetros da música.
 function geraString(){
-     this.parametros="python composer.py "+tipo+" "+cenario+" "+duracao+" "+instrumento;
+     this.parametros="python composer.py "+tipo+" "+cenario+" "+duracao+" "+instrumento+" "+andamento;;
+     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+fetch('http://localhost:3000',{method:'POST',body:'Humongous dong'}).then(results=>results.json()).then(console.log);
+
 }
+
 
 /*
 function teste2(){
@@ -33,4 +47,3 @@ function teste2(){
 	teste.innerHTML=parametros;
 }
 */
-
