11e90d339144f744b3627d38e2569aa6dae88be1
LucasAMoreira <39498462+LucasAMoreira@users.noreply.github.com>
2020-12-07 21:58:08 -0300

    Adição da caixa com resposta

--- gerar.js
+++ gerar.js
@@ -33,7 +33,24 @@ function geraString(){
      this.parametros="python composer.py "+genero+" "+instrumento+" "+andamento;
      const proxyurl = "https://cors-anywhere.herokuapp.com/";
 fetch('http://localhost:3000',{method:'POST',body:'Humongous dong'}).then(results=>results.json()).then(console.log);
+}
 
+function retornaLink(){
+     //link com a música
+     var link="http:192.168.1.102:420/song.mid";
+
+     // Código html (dividido só para facilitar escrita)
+     var AbreDiv='<div class="col-12 bg-light" style="border-radius:5pt">';
+     var h4 ='\n<h4 class="p-3 text-center">\nSample Gerada!\n</h4>\n';
+     var texto='<p class="text-center pb-3">Clique <a href="'+link+'"">aqui</a> para ser redirecionado para página com o arquivo\n</p>';
+     var fechaDiv='</div>';
+
+     // Junta código HTML
+     var caixaResposta = AbreDiv+h4+texto+fechaDiv;
+
+     // Exibe caixa e link para o usuário
+     var resposta = document.getElementById('musica');
+     resposta.innerHTML=caixaResposta;
 }
 
 
@@ -41,5 +58,5 @@ fetch('http://localhost:3000',{method:'POST',body:'Humongous dong'}).then(result
 function teste2(){
 	var teste = document.getElementById("teste2");
 	teste.innerHTML=parametros;
-}
-*/
+}*/
+
